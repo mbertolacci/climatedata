474f670c9b28e82151c90860156100d6f6e9b4e7
Michael Bertolacci <m.bertolacci@gmail.com>
2018-04-24 09:06:51 +0800

    Update documentation, make get_for_dates more generic

--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,16 @@
     return(c(year, month + 1))
 }
 
+#' Get rows for the given dates
+#'
+#' Returns rows matching the provided dates.
+#'
+#' @param data Data, typically loaded using load_soi/iod/sam, for which the
+#' first column is an integer year and the second column is an integer month
+#' @param dates Dates to grab corresponding rows for. Can be any type
+#' supporting the format command (e.g., Date, POSIXlt, POSIXct)
+#' @param lags Number of months to lag matching. For instance, lag = 1
+#' corresponds to matching to date to the previous month in the data.
 #' @export
 get_for_dates <- function(data, dates, lag = 0) {
     data_dates <- cbind(data$year, data$month)
@@ -14,7 +24,7 @@ get_for_dates <- function(data, dates, lag = 0) {
         data_dates[i, ] <- .add_lag(data_dates[i, 1], data_dates[i, 2], lag)
     }
     data[match(
-        100 * (dates$year + 1900) + dates$mon + 1,
-        100 * data_dates[, 1] + data_dates[, 2]
+        format(dates, '%Y-%m'),
+        sprintf('%04d-%02d', data_dates[, 1], data_dates[, 2])
     ), ]
 }
